89e957b3e9bd1045f4d0873b99e071be495d6fd0
WilliamArantes <williamarantes.tec@gmail.com>
2023-12-14 12:23:12 -0300

    Aula do dia 28-12-2023 Configuração inicial de conexão com o banco de dados postgres

---
+++ MESTTRA/ecommerce-mesttra/back-end/dbConfig.js
@@ -0,0 +1,22 @@
+//Importando o Pool do posrgres-note (Como Pool é um objeto, queremos pegar somente um elemento)
+const {Pool} = require('pg')
+
+//Iniciando a minha classe de configuração do pool de conexão
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'ecommercemesttra',
+    password: '@1630220',
+    port: 5432
+})
+
+//teste para saber se a conexão com o banco deu certo (retorna o nome das tabelas e etc...)
+const teste = async() =>{
+    const result = await pool.query('SELECT * FROM products')
+    console.log(result.rows[0])
+}
+
+teste()
+
+//exportando a minha constante pool
+module.exports = pool
